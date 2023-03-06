c7b0d7b2077d00de8c098430741beeafa0cc3295
paul-pelith <paul.li@pelith.com>
2023-03-06 18:58:00 +0800

    fix: scan-index

--- src/utils/getERC721EnumerableScan.ts
+++ src/utils/getERC721EnumerableScan.ts
@@ -1,7 +1,9 @@
+import { BigNumber } from 'ethers'
 import range from 'lodash/range'
 import { erc721ABI } from 'wagmi'
-import { readContract } from 'wagmi/actions'
+import { multicall, readContract } from 'wagmi/actions'
 
+import ABINFTSample from '@/constants/abis/ABINFTSample'
 import { nftWalletsStore } from '@/store/nftWallet'
 
 export default async function getERC721EnumerableScan(
@@ -15,9 +17,19 @@ export default async function getERC721EnumerableScan(
       functionName: 'balanceOf',
       args: [address],
     })
-    const rangeOfIndex = range(1, result.toNumber() - 1)
-    for await (const result of rangeOfIndex.map((i) =>
-      nftWalletsStore.set.addNFTAddress(nftAddress, i),
+    const rangeOfIndex = range(0, result.toNumber()).map((i) => BigNumber.from(i))
+
+    const _ownedIds = await multicall({
+      contracts: rangeOfIndex.map((id) => ({
+        address: nftAddress,
+        abi: ABINFTSample,
+        functionName: 'tokenOfOwnerByIndex',
+        args: [address, id],
+      })),
+    })
+
+    for await (const result of (_ownedIds as BigNumber[]).map((i) =>
+      nftWalletsStore.set.addNFTAddress(nftAddress, i.toNumber()),
     )) {
       console.log(result)
     }
