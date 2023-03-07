20626338c23a882ec63e8b3fda02636ccdae42c5
timwang <timwang@pelith.com>
2023-03-07 16:49:26 +0800

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import nodePolyfills from 'rollup-plugin-polyfill-node'
 import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: any }) => {
   const env = loadEnv(mode, process.cwd()).VITE_APP_ENV
 
   return defineConfig({
